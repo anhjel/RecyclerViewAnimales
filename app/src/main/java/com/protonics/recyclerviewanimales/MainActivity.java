0da05c0a61a1f4d4ec9d6996b62081b4c42b8bc1
anhjel <anhjel@hotmail.com>
2016-10-06 20:20:54 -0500

    punto2

--- app/src/main/java/com/protonics/recyclerviewanimales/MainActivity.java
+++ app/src/main/java/com/protonics/recyclerviewanimales/MainActivity.java
@@ -36,15 +36,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //----------------------------
-
         toolbar = (Toolbar) findViewById(R.id.toolBar);
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         viewPager = (ViewPager)  findViewById(R.id.viewPager);
-
         setUpViewPager();
-
-
         if (toolbar != null){
             setSupportActionBar(toolbar);
         }
@@ -100,6 +95,8 @@ public class MainActivity extends AppCompatActivity {
         return fragments;
 
     }
+
+
     private  void  setUpViewPager(){
 
         viewPager.setAdapter(new PageAdapter(getSupportFragmentManager(),agregarFragments()));
